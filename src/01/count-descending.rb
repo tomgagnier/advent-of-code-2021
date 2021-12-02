22fbbb80d675ebc74df2a0c78a72345ffc0e64b0
Tom Gagnier (tgagnier) <tgagnier@cisco.com>
2021-12-01 21:54:37 -0500

    inline read_lines

--- src/01/count-descending.rb
+++ src/01/count-descending.rb
@@ -1,11 +1,10 @@
-require '../aoc.rb'
-
 def count_descending(integers)
   integers[..-2].zip(integers[1..]).filter { |l, r| l < r }.count
 end
 
-def process(input)
-  depths = read_lines(input).map(&:to_i)
+def process(input_file)
+  depths = File.readlines(input_file).map(&:strip).reject(&:empty?).map(&:to_i)
+
   p count_descending(depths)
 
   sliding_window_sums = depths[..-3].zip(depths[1..-2], depths[2..]).map(&:sum)
