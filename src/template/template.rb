3c75402553de607a5253af0b095b88d1dbff86d4
Tom Gagnier (tgagnier) <tgagnier@cisco.com>
2021-12-03 15:10:12 -0500

    Add starting template

---
+++ src/template/template.rb
@@ -0,0 +1,18 @@
+def part_1(lines)
+  'incomplete'
+end
+
+def part_2(lines)
+  'incomplete'
+end
+
+def process(input_file)
+  lines = File.readlines(input_file).map(&:strip).reject(&:empty?)
+
+  puts "#{input_file}: 1) #{part_1(lines)} 2) #{part_2(lines)}"
+end
+
+process('test.txt')
+
+#%w(test.txt input.txt).each { |f| process(f) }
+#
