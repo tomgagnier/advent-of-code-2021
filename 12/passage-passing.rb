12b2c7cd8260b83abf6e79effefe34d3c9da0b21
Tom Gagnier (tgagnier) <tgagnier@cisco.com>
2021-12-12 11:38:57 -0500

    Refactor day 11

--- 12/passage-passing.rb
+++ 12/passage-passing.rb
@@ -1,37 +1,24 @@
-def duplicate_small_cave_count(path)
-  small_caves = path.reject { |n| /^[A-Z]+$/.match?(n) }
-  small_caves.size - small_caves.uniq.size
-end
-
-def paths(connections, constraint, candidates = [['start']], paths = [])
+def paths(connections, constraint, paths = [], candidates = [['start']])
   return paths if candidates.empty?
   candidates, ended = candidates.partition { |path| path[-1] != 'end' }
-  candidates.select! { |path| constraint.call(path) }
-  candidates.map! { |path| connections[path[-1]].map { |node| path + [node] } }
-  paths(connections, constraint, candidates.flatten(1), paths + ended)
+  paths(connections, constraint, paths + ended,
+        candidates.select { |path| constraint.call(path) }
+                  .map { |path| connections[path[-1]].map { |node| path + [node] } }
+                  .flatten(1))
+end
+
+def duplicate_small_cave_count(path)
+  path.reject { |n| n == n.upcase }.tally.values.map { |v| v - 1 }.sum
 end
 
 def process(input)
   connections = File.readlines(input).map(&:strip).reject(&:empty?).map { |l| l.split('-') }
-                    .reduce(Hash.new { |h, k| h[k] = [] }) { |h, c|
-                      if c[0] == 'end'
-                        h[c[1]] << c[0]
-                      elsif c[1] == 'end'
-                        h[c[0]] << c[1]
-                      elsif c[0] == 'start'
-                        h[c[0]] << c[1]
-                      elsif c[1] == 'start'
-                        h[c[1]] << c[0]
-                      else
-                        h[c[0]] << c[1]
-                        h[c[1]] << c[0]
-                      end
-                      h }
-
-  part_1 = paths(connections, lambda { |path| duplicate_small_cave_count(path) == 0 }).size
-  part_2 = paths(connections, lambda { |path| duplicate_small_cave_count(path) <= 1 }).size
-
-  puts " #{input}: \n\t1) #{part_1}\n\t2) #{part_2}"
+                    .map { |connection| [connection, connection.reverse] }.flatten(1)
+                    .reject { |l, r| l == 'end' || r == 'start' }
+                    .reduce(Hash.new { |h, k| h[k] = [] }) { |h, p| h[p[0]] << p[1]; h }
+  puts "#{input}:"
+  puts "\t1) #{paths(connections, lambda { |path| duplicate_small_cave_count(path) < 1 }).size}"
+  puts "\t2) #{paths(connections, lambda { |path| duplicate_small_cave_count(path) < 2 }).size}"
 end
 
-%w(test1.txt test2.txt test3.txt input.txt).each{|file| process(file)}
+%w(test1.txt test2.txt test3.txt input.txt).each { |file| process(file) }
