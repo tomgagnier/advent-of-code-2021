9c48a872d92824b48c9a8a4b3de6f774502a4484
Tom Gagnier (tgagnier) <tgagnier@cisco.com>
2021-12-12 22:05:42 -0500

    Refactor day 12

--- 12/passage-passing.rb
+++ 12/passage-passing.rb
@@ -1,8 +1,8 @@
-def paths(connections, constraint, paths = [], candidates = [['start']])
+def paths(connections, is_ended, is_valid, paths = [], candidates = [['start']])
   return paths if candidates.empty?
-  candidates, ended = candidates.partition { |path| path[-1] != 'end' }
-  paths(connections, constraint, paths + ended,
-        candidates.select { |path| constraint.call(path) }
+  ended, candidates = candidates.partition { |path| is_ended.call(path) }
+  paths(connections, is_ended, is_valid, paths + ended,
+        candidates.select { |path| is_valid.call(path) }
                   .map { |path| connections[path[-1]].map { |node| path + [node] } }
                   .flatten(1))
 end
@@ -12,13 +12,14 @@ def duplicate_small_cave_count(path)
 end
 
 def process(input)
+  puts "#{input}:"
   connections = File.readlines(input).map(&:strip).reject(&:empty?).map { |l| l.split('-') }
                     .map { |connection| [connection, connection.reverse] }.flatten(1)
                     .reject { |l, r| l == 'end' || r == 'start' }
                     .reduce(Hash.new { |h, k| h[k] = [] }) { |h, p| h[p[0]] << p[1]; h }
-  puts "#{input}:"
-  puts "\t1) #{paths(connections, lambda { |path| duplicate_small_cave_count(path) < 1 }).size}"
-  puts "\t2) #{paths(connections, lambda { |path| duplicate_small_cave_count(path) < 2 }).size}"
+  is_ended = lambda{|path| path[-1] == 'end'}
+  puts "\t1) #{paths(connections, is_ended, lambda { |path| duplicate_small_cave_count(path) < 1 }).size}"
+  puts "\t2) #{paths(connections, is_ended, lambda { |path| duplicate_small_cave_count(path) < 2 }).size}"
 end
 
 %w(test1.txt test2.txt test3.txt input.txt).each { |file| process(file) }
